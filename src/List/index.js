2584cb5a02d6416591842b92426a6b964bed573c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-06-10 16:13:04 +0200

    list of currency added

--- src/List/index.js
+++ src/List/index.js
@@ -1,58 +1,66 @@
+import { useState } from "react";
 import "./style.css";
 
-const List = () => (
-    <ul className="list">
-        <li className="list__item">
-            <div className="list__inputContainer">
-                <input type="radio" name="currencyChecked" id="eur" value="EUR" checked />
-                <label className="list__label" for="eur">
-                    EUR Euro
-                                </label>
-            </div>
-            <input className="list__input" value={4.5481} type="number" min="0.0001" step="0.0001"
-                required />
-        </li>
-        <li className="list__item">
-            <div className="list__inputContainer">
-                <input type="radio" name="currencyChecked" id="usd" value="USD" />
-                <label className="list__label" for="usd">
-                    USD Dolar amerykański
-                                </label>
-            </div>
-            <input className="list__input" value={3.7978} type="number" min="0.0001" step="0.0001"
-                required />
-        </li>
-        <li className="list__item">
-            <div className="list__inputContainer">
-                <input type="radio" name="currencyChecked" id="aud" value="AUD" />
-                <label className="list__label" for="aud">
-                    AUD Dolar australijski
-                                </label>
-            </div>
-            <input className="list__input" value={2.8517} type="number" min="0.0001" step="0.0001"
-                required />
-        </li>
-        <li className="list__item">
-            <div className="list__inputContainer">
-                <input type="radio" name="currencyChecked" id="bgn" value="BGN" />
-                <label className="list__label" for="bgn">
-                    BGN Lew bułgarski
-                                </label>
-            </div>
-            <input className="list__input" value={2.3254} type="number" min="0.0001" step="0.0001"
-                required />
-        </li>
-        <li className="list__item">
-            <div className="list__inputContainer">
-                <input type="radio" name="currencyChecked" id="hrk" value="HRK" />
-                <label className="list__label" for="hrk">
-                    HRK Kuna chorwacka
-                                </label>
-            </div>
-            <input className="list__input" value={0.6011} type="number" min="0.0001" step="0.0001"
-                required />
-        </li>
-    </ul>
-);
+const List = () => {
+    const [currencyTable, setCurrencyTable] = useState([
+        {
+            key: "EUR",
+            value: 4.5481,
+            label: "Euro",
+        },
+        {
+            key: "USD",
+            value: 3.7978,
+            label: "Dolar amerykański",
+        },
+        {
+            key: "AUD",
+            value: 2.8517,
+            label: "Dolar australijski",
+        },
+        {
+            key: "BGN",
+            value: 2.3254,
+            label: "Lew bułgarski",
+        },
+        {
+            key: "HRK",
+            value: 0.6011,
+            label: "Kuna chorwacka",
+        }
+    ]);
+
+    return (
+        <ul className="list">
+            {currencyTable.map(currency => (
+                <li className="list__item">
+                    <div className="list__inputContainer">
+                        <input
+                            type="radio"
+                            name="currencyList"
+                            id={currency.key}
+                            value={currency.key}
+                            checked
+                        />
+                        <label
+                            className="list__label"
+                            for={currency.key}>
+                            {currency.label}
+                        </label>
+                    </div>
+                    <input
+                        className="list__input"
+                        value={currency.value}
+                        type="number"
+                        min="0.0001"
+                        step="0.0001"
+                        required
+                    />
+                </li>
+            ))
+            };
+        </ul>
+    )
+};
 
 export default List;
