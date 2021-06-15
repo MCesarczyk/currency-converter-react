916f47840e7efe0e4842aba87cf22b708fc3eb1f
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-06-15 23:28:38 +0200

    logic to clock added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 
-const Clock = () => (
-    <div className="form__clockContainer" >
-                    <p className="form__clockElement">
-                        wtorek, 15 czerwca, 21:27:29
-                    </p>
-                </div>
-);
+const Clock = () => {
+
+    const [customDateWithTime, setCustomDateWithTime] = useState("");
+
+    useEffect(() => {
+        getCustomDate();
+        const clockInterval = setInterval(() => {
+            getCustomDate();
+        }, 1000);
+
+        return () => {
+            clearInterval(clockInterval);
+        };
+    }, []);
+
+    const getCustomDate = () => {
+        const myDate = new Date();
+        const customDate = myDate.toLocaleString(
+            undefined,
+            { weekday: "long", day: "numeric", month: "long", time: "numeric" },
+        );
+        const time = myDate.toLocaleTimeString();
+
+        setCustomDateWithTime(`${customDate}, ${time}`);
+    };
+
+    return (
+        <div className="form__clockContainer" >
+            <p className="form__clockElement">
+                {customDateWithTime}
+            </p>
+        </div>
+    );
+};
 
 export default Clock;
