8891bb80e326aebf677fd61db5ed10e82191d2c1
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-07-09 19:54:16 +0200

    destructuring data fetched from API, add /tmp to .gitignore

--- src/App/Form/useCurrentRates.js
+++ src/App/Form/useCurrentRates.js
@@ -2,31 +2,27 @@ import { useEffect, useState } from "react";
 
 export const useCurrentRates = () => {
     const [ratesData, setRatesData] = useState({
-        success: false,
-        error: false,
+        status: "loading",
+        base: "PLN",
         date: null,
         rates: null
     });
 
     useEffect(() => {
         const getRates = () => {
-            try {
-                const request = new XMLHttpRequest();
-                const requestURL = 'https://api.exchangerate.host/latest';
-                const base = "base=PLN";
+            const request = new XMLHttpRequest();
+            const requestURL = 'https://api.exchangerate.host/latest';
+            const base = `base=${ratesData.base}`;
 
-                request.open('GET', requestURL + "?" + base, true);
-                request.responseType = 'json';
+            request.open('GET', requestURL + "?" + base, true);
+            request.responseType = 'json';
 
-                request.onload = () => {
-                    setRatesData(request.response);
-                };
+            request.onload = () => {
+                const { date, rates } = request.response;
+                setRatesData({ status: "success", base, date, rates });
+            };
 
-                request.send(null);
-
-            } catch (error) {
-                setRatesData({ error: true });
-            }
+            request.send(null);
         };
 
         setTimeout(getRates, 2_000);
