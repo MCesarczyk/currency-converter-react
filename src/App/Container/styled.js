ba67cbd2cda78947264113f1e2549a6df3d3ad3f
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-07-18 00:12:23 +0200

    image full width set

--- src/App/Container/styled.js
+++ src/App/Container/styled.js
@@ -2,8 +2,7 @@ import styled from "styled-components";
 import background from "../../images/background.png";
 
 export const ContainerBody = styled.div`
-    max-width: 1024px;
-    margin: 0 auto;
+    /* margin: 0 auto; */
     background-image: url("${background}");
     background-size: cover;
     background-position: center;
@@ -12,7 +11,7 @@ export const ContainerBody = styled.div`
 `;
 
 export const ContainerElement = styled.div`
-    max-width: ${({ theme }) => theme.breakpoint.mobileMax};
+    max-width: 360px;
     min-width: 240px;
     margin: 0 auto;
     background-color: #eee;
