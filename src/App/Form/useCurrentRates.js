6a6eb610962d2de9322dec44bfdb828a10df320d
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-07-25 12:44:49 +0200

    changing .then construction to async await

--- src/App/Form/useCurrentRates.js
+++ src/App/Form/useCurrentRates.js
@@ -12,28 +12,28 @@ export const useCurrentRates = () => {
         const getRates = () => {
             const requestURL = 'https://api.exchangerate.host/latest';
 
-            fetch(requestURL + "?base=" + ratesData.base)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP Error! Status: ${response.status}`);
-                    }
-                    return response;
-                })
-                .then(response => response.json())
-                .then(
-                    response => {
-                        setRatesData({
-                            status: "success",
-                            base: response.base,
-                            date: response.date,
-                            rates: response.rates
-                        });
-                    }
-                )
+            const dataFetch = async () => {
+                let response = await fetch(requestURL + "?base=" + ratesData.base);
+
+                if (!response.ok) {
+                    throw new Error(`HTTP Error! Status: ${response.status}`);
+                }
+
+                response = await response.json();
+                
+                setRatesData({
+                    status: "success",
+                    base: response.base,
+                    date: response.date,
+                    rates: response.rates
+                });
+            };
+
+            dataFetch()
                 .catch(error => {
                     setRatesData({ status: "error" });
                     console.error(error.message);
-                })
+                });
         };
 
         setTimeout(getRates, 2_000);
