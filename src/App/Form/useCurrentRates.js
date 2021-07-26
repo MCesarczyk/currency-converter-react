bb41e83d42816f39e3fb44fe3b90b60d0917d7d2
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-07-27 00:51:18 +0200

    added handling for rates and base errors

--- src/App/Form/useCurrentRates.js
+++ src/App/Form/useCurrentRates.js
@@ -1,40 +1,49 @@
 import { useEffect, useState } from "react";
 
 export const useCurrentRates = () => {
-    const [ratesData, setRatesData] = useState({
-        status: "loading",
-        base: "PLN",
-        date: null,
-        rates: null
-    });
+  const [ratesData, setRatesData] = useState({
+    status: "loading",
+    base: "PLN",
+    date: null,
+    rates: null
+  });
 
-    const requestURL = 'https://api.exchangerate.host/latest';
+  const requestURL = 'https://api.exchangerate.host/latest';
 
-    useEffect(() => {
-        const getRates = async () => {
-            try {
-                const response = await fetch(requestURL + "?base=" + ratesData.base);
+  useEffect(() => {
+    const getRates = async () => {
+      try {
+        const response = await fetch(requestURL + "?base=" + ratesData.base);
 
-                if (!response.ok) {
-                    throw new Error(`HTTP Error! Status: ${response.status}`);
-                }
+        if (!response.ok) {
+          throw new Error("No response");
+        }
 
-                const { base, date, rates } = await response.json();
+        const { base, date, rates } = await response.json();
 
-                setRatesData({
-                    status: "success",
-                    base: base,
-                    date: date,
-                    rates: rates
-                });
+        if (!rates) {
+          throw new Error("No rates data");
+        }
 
-            } catch (error) {
-                setRatesData({ status: "error" });
-            }
-        };
+        if (base !== ratesData.base) {
+          throw new Error(`Wrong currency base: ${base}`);
+        }
 
-        setTimeout(getRates, 2_000);
-    }, []);
+        setRatesData({
+          status: "success",
+          base: base,
+          date: date,
+          rates: rates
+        });
 
-    return ratesData;
+      } catch (error) {
+        setRatesData({ status: "error" });
+        console.log(error.message);
+      }
+    };
+
+    setTimeout(getRates, 2_000);
+  }, []);
+
+  return ratesData;
 };
